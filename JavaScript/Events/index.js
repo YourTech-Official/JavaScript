570d2f2481eff162516e8d4d87acb5dc93a2e12a
YourTech-Official <contact.yourtech.official@gmail.com>
2025-12-02 09:41:57 +0530

    Add files via upload

---
+++ JavaScript/Events/index.js
@@ -0,0 +1,6 @@
+// Event - an event is an action or occurance that happens in the braowser,
+// usually as a result of user interaction or the browser's system processes.
+
+// EventHandling - this is a process of responding to user interactions or occurances in the browser,
+// such as click, key presses, or form submissions.
+
